6da9e3a5aaa9e06001939eb34b6f71a2fe86ca4b
Raffi Kudlac <raffi.kudlac@gmail.com>
2019-06-18 12:41:21 -0700

    added ajax and search errors

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,9 +2,9 @@ class StocksController < ApplicationController
 
   def search
     @stock = Stock.new_from_lookup(params[:stock])
-    if (!@stock)
-      flash[:danger] = "Invalid  search parameters"
+    flash.now[:danger] = "Invalid  search parameters" if !@stock    
+    respond_to do |format|
+      format.js {render partial: 'users/result'}
     end
-    render my_portfolio_path
   end
 end
